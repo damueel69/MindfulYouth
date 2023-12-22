311fda28a928cfc41851c3bddb0366a14e864aad
ziafauzia <151479325+ziafauzia@users.noreply.github.com>
2023-12-22 19:48:42 +0700

    dashboard.php

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -1,65 +1,54 @@
-<?php
-// Lakukan koneksi ke database
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "mindfulyouth";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+<div class="container-fluid"> 
+    <h1 class="h3 mb-4 text-gray-800"><?= $judul; ?></h1>
+    <div class="row">
+        <div class="col-md-6"><a href="<?= base_url() ?>Admin/tambah" class="btn btn-info mb-2">Tambah Data</a></div>
+        <div class="col-md-12">
+            <?= $this->session->flashdata('message');?>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <td>No</td>
+                        <td>Username</td>
+                        <td>Nama</td>
+                        <td>Email</td>
+                        <td>Role</td>
+                        <td>No HP</td>
+                        <td>Specialist</td>
+                        <td>Gelar</td>
+                        <td>Aksi</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php $i = 1; ?>
+                    
+                    <?php if (!empty($data['users'])): ?>
+                       <?php foreach ($data['users'] as $us): ?>
+                            <tr>
+                                <td><?= $i; ?>.</td>
+                                <td><?= $us['username']; ?></td>
+                                <td><?= $us['nama']; ?></td>
+                                <td><?= $us['email']; ?></td>
+                                <td><?= $us['role']; ?></td>
+                                <td><?= $us['no_hp']; ?></td>
+                                <td><?= $us['specialist']; ?></td>
+                                <td><?= $us['gelar']; ?></td>
+                                <td>
+                                    <a href="<?= base_url('Admin/hapus/') . $us['id_user']; ?>" class="btn btn-danger">Hapus</a>
+                                    <a href="<?= base_url('Admin/edit/') . $us['id_user']; ?>" class="btn btn-warning">Edit</a>
+                                    <a href="<?= base_url('Admin/detail/') . $us['id_user']; ?>" class="btn btn-info">Detail</a>
+                                </td>
+                            </tr>
+                            <?php $i++; ?>
+                        <?php endforeach; ?>
+                    <?php else: ?>
+                        <tr>
+                            <td colspan="9">No data available</td>
+                        </tr>
+                    <?php endif; ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
 
-// Periksa koneksi
-if ($conn->connect_error) {
-    die("Koneksi gagal: " . $conn->connect_error);
-}
-
-// Jika ada data yang diubah dan dikirim dari form
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Pastikan variabel yang diterima aman untuk diproses dalam query SQL
-    $id = $_POST['id'];
-    $username = $_POST['username'];
-    $nama = $_POST['nama'];
-    $email = $_POST['email'];
-    $role = $_POST['role'];
-    $no_hp = $_POST['no_hp'];
-    $specialist = $_POST['specialist'];
-    $gelar = $_POST['gelar'];
-
-    // Lakukan query update berdasarkan ID
-    $sql = "UPDATE users SET username='$username', nama='$nama', email='$email', role='$role', no_hp='$no_hp', specialist='$specialist', gelar='$gelar' WHERE id_user=$id";
-
-    if ($conn->query($sql) === TRUE) {
-        echo "Data berhasil diperbarui";
-    } else {
-        echo "Error updating record: " . $conn->error;
-    }
-}
-
-// Ambil semua data dari tabel users
-$sql = "SELECT * FROM users";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    // Tampilkan tabel untuk menampilkan data dan form untuk mengedit
-    echo "<table border='1'>";
-    echo "<tr><th>ID</th><th>Username</th><th>Nama</th><th>Email</th><th>Role</th><th>No HP</th><th>Specialist</th><th>Gelar</th><th>Edit</th></tr>";
-    while($row = $result->fetch_assoc()) {
-        echo "<form method='post'>";
-        echo "<tr>";
-        echo "<td><input type='hidden' name='id' value='". $row["id_user"] ."'>" . $row["id_user"] . "</td>";
-        echo "<td><input type='text' name='username' value='". $row["username"] ."'></td>";
-        echo "<td><input type='text' name='nama' value='". $row["nama"] ."'></td>";
-        echo "<td><input type='text' name='email' value='". $row["email"] ."'></td>";
-        echo "<td><input type='text' name='role' value='". $row["role"] ."'></td>";
-        echo "<td><input type='text' name='no_hp' value='". $row["no_hp"] ."'></td>";
-        echo "<td><input type='text' name='specialist' value='". $row["specialist"] ."'></td>";
-        echo "<td><input type='text' name='gelar' value='". $row["gelar"] ."'></td>";
-        echo "<td><input type='submit' value='Simpan'></td>";
-        echo "</tr>";
-        echo "</form>";
-    }
-    echo "</table>";
-} else {
-    echo "0 results";
-}
-$conn->close();
-?>
