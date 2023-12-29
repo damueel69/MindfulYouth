c0adc52b109ba95eb253777acbc22b2e525561e1
damueel69 <151478971+damueel69@users.noreply.github.com>
2023-12-29 22:38:46 +0700

    application

---
+++ application/views/artikel/daftar_artikel_worker.php
@@ -0,0 +1,49 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Daftar Artikel</title>
+</head>
+<body>
+    <div id="kategori">
+        <?php
+        echo anchor('artikel/stress', 'Stress');
+        ?>
+        <?php
+        echo anchor('artikel/depresi', 'Depresi');
+        ?>
+         <?php
+        echo anchor('artikel/kecemasan', 'Kecemasan');
+        ?>
+        <?php
+        echo anchor('artikel/gKepribadian', 'Gangguan Kepribadian');
+        ?>
+         <?php
+        echo anchor('artikel/gTidur', 'Gangguan Tidur');
+        ?>
+        <?php
+        echo anchor('artikel/rumah_tangga', 'Rumah Tangga');
+        ?>
+        <!-- Tambahkan link kategori lainnya di sini -->
+    </div>
+    <?php
+        echo anchor('artikel/create', 'Tambah Artikel');
+        ?>
+
+    <div id="daftar-artikel">
+        <!-- Daftar artikel akan dimuat di sini -->
+        <ul>
+            <?php foreach ($articles as $article) { ?>
+                <li>
+                <h2><?php echo anchor('artikel/detail/' . $article->id, $article->judul); ?></h2>
+                <p><?php echo substr($article->konten, 0, 20); ?></p>
+                    <p><strong>Tanggal Publikasi:</strong> <?php echo $article->tanggal_publikasi; ?></p>
+                    <p><strong>Kategori:</strong> <?php echo $article->kategori; ?></p>
+                </li>
+            <?php } ?>
+        </ul>
+    </div>
+</body>
+</html>
---
+++ application/views/chat/group/index.php
@@ -0,0 +1,62 @@
+<h2>Daftar Chat Group</h2>
+<?php echo anchor('group/add', 'Buat Chat Group', ['class' => 'btn btn-primary btn-sm']); ?>
+<br />
+<br />
+<table class="table table-bordered">
+  <tr>
+    <thead>
+      <tr>
+        <th>No</th>
+        <th>Nama Grup</th>
+        <th>Dibuat Oleh</th>
+        <th>Jumlah Member</th>
+        <th>Aksi</th>
+      </tr>
+    </thead>
+  </tr>
+  <tbody>
+  <?php
+  $no = 0;
+  foreach ($record->result() as $r) {
+    $no++;
+    echo "<tr>
+      <td>$no</td>
+      <td>$r->topic</td>
+      <td>$r->created_by</td>
+      <td>$r->total_member</td>
+      <td>".
+        anchor('group/check/'. $r->chat_id, 'Chat', ['class' => 'btn btn-success btn-sm'])
+      ."</td>
+    </tr>";
+  }
+  ?>
+  </tbody>
+</table>
+</p>
+
+<script src="<?php echo base_url(); ?>js/dashboard.js" type="text/javascript"></script>
+
+<div class="col-md-4">
+    <div class="card mb-4">
+        <img src="<?= base_url('assets/assets/') ?>images/kanselor.jpg" class="card-img-top" alt="Konselor Image">
+        <div class="card-body">
+            <h2 class="card-title">Chat Konselor</h2>
+            <p class="card-text">Konselor siap membantu Anda dengan berbagai permasalahan kehidupan sehari-hari, memberikan dukungan emosional, serta bantuan dalam menghadapi situasi yang sulit.</p>
+            <?php echo anchor('group/kanselor', 'Pilih Spesialist Kanselor', ['class' => 'btn btn-primary btn-sm']); ?>
+        </div>
+    </div>
+</div>
+<div class="col-md-4">
+    <div class="card mb-4">
+        <img src="<?= base_url('assets/assets/') ?>images/psikitiater.png" class="card-img-top" alt="Psikiater Image">
+        <div class="card-body">
+            <h2 class="card-title">Chat Psikiater</h2>
+            <p class="card-text">Psikiater adalah tenaga medis yang memiliki keahlian khusus dalam menangani masalah kesehatan mental, diagnosis, serta pengobatan. Mereka dapat memberikan layanan konsultasi dan terapi khusus untuk masalah kejiwaan.</p>
+            <?php if ($this->session->userdata('role') == 'Admin'): ?>
+                <a href="<?= base_url()?>Chat/" class="btn btn-primary">Pilih Psikiater</a>
+            <?php else: ?>
+                <?php echo anchor('group/psikiater', 'Pilih Spesialist Psikiater', ['class' => 'btn btn-primary btn-sm']); ?>
+            <?php endif; ?>
+        </div>
+    </div>
+</div>
