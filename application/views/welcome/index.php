83edbb24339a2c0f9f47566a712017cece679787
damueel69 <151478971+damueel69@users.noreply.github.com>
2023-12-29 22:40:28 +0700

    Delete application directory

--- application/views/welcome/index.php
+++
@@ -1,7 +0,0 @@
-<p align="center">
-  <h2 align="center">
-    SELAMAT DATANG DI APLIKASI MIFUYU INDONESIA <br />
-    SILAHKAN LOGIN TERLEBIH DAHULU SEBELUM <br />
-    MENJALANKAN APLIKASI INI <br />
-  </h2>
-</p>
