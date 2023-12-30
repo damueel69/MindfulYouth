3d340a4c4f4fdc03647b2ea2951fa178efe34800
ziafauzia <151479325+ziafauzia@users.noreply.github.com>
2023-12-30 14:41:26 +0700

    index.php

--- application/views/welcome/index.php
+++ application/views/welcome/index.php
@@ -1,4 +1,5 @@
 <p align="center">
+  <img src="<?php echo base_url(); ?>assets/images/logo.jpg"  width="400" height="400">
   <h2 align="center">
     SELAMAT DATANG DI APLIKASI MIFUYU INDONESIA <br />
     SILAHKAN LOGIN TERLEBIH DAHULU SEBELUM <br />
