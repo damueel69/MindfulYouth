f8973d448b57fc3df0f5c3d5ee14d5777135ca80
damueel69 <151478971+damueel69@users.noreply.github.com>
2024-01-08 08:11:05 +0700

    Add files via upload

--- application/views/artikel/detail_artikel.php
+++ application/views/artikel/detail_artikel.php
@@ -14,31 +14,30 @@
             max-width: 800px;
             margin: 0 auto;
             padding: 20px;
-            background-color: #fff;
+            background-color: #5C8374; /* Warna latar belakang baru */
             border-radius: 8px;
             box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
         }
         h1 {
             text-align: center;
-            color: #333;
-            font-size: 32px; /* Ukuran font judul */
+            color: #fff; /* Warna teks judul */
+            font-size: 48px; /* Ukuran font judul yang lebih besar */
         }
         h2 {
-            color: #444;
-            font-size: 26px; /* Ukuran font subjudul */
+            color: #fff; /* Warna teks subjudul */
+            font-size: 36px; /* Ukuran font subjudul yang lebih besar */
         }
         p {
-            margin-bottom: 15px;
-            font-size: 18px; /* Ukuran font paragraf */
+            margin-bottom: 20px; /* Jarak antar paragraf yang sedikit lebih besar */
+            font-size: 24px; /* Ukuran font paragraf yang lebih besar */
         }
         strong {
-            color: #333;
+            color: #fff; /* Warna teks tebal */
         }
     </style>
 </head>
 <body>
-    <div class="article"
-    style="background-color: #5C8374">
+    <div class="article">
         <?php if(isset($article)) : ?>
             <h1><?php echo $article->judul; ?></h1>
             <p><strong>Tanggal Publikasi:</strong> <?php echo $article->tanggal_publikasi; ?></p>
