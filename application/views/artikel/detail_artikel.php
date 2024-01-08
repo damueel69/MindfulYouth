c94538ffdecf9c4bbf00ce00536af4c87f8deb2c
damueel69 <151478971+damueel69@users.noreply.github.com>
2024-01-08 08:01:13 +0700

    Add files via upload

--- application/views/artikel/detail_artikel.php
+++ application/views/artikel/detail_artikel.php
@@ -3,14 +3,51 @@
 <head>
     <meta charset="UTF-8">
     <title>Detail Artikel</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            line-height: 1.6;
+            color: #333;
+            margin: 20px;
+        }
+        .article {
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 20px;
+            background-color: #fff;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+        h1 {
+            text-align: center;
+            color: #333;
+            font-size: 32px; /* Ukuran font judul */
+        }
+        h2 {
+            color: #444;
+            font-size: 26px; /* Ukuran font subjudul */
+        }
+        p {
+            margin-bottom: 15px;
+            font-size: 18px; /* Ukuran font paragraf */
+        }
+        strong {
+            color: #333;
+        }
+    </style>
 </head>
 <body>
-    <h1>Detail Artikel</h1>
-    <div>
-        <h2><?php echo $article->judul; ?></h2>
-        <p><?php echo $article->konten; ?></p>
-        <p><strong>Tanggal Publikasi:</strong> <?php echo $article->tanggal_publikasi; ?></p>
-        <p><strong>Kategori:</strong> <?php echo $article->kategori; ?></p>
+    <div class="article"
+    style="background-color: #5C8374">
+        <?php if(isset($article)) : ?>
+            <h1><?php echo $article->judul; ?></h1>
+            <p><strong>Tanggal Publikasi:</strong> <?php echo $article->tanggal_publikasi; ?></p>
+            <p><strong>Kategori:</strong> <?php echo $article->kategori; ?></p>
+            <hr>
+            <p><?php echo $article->konten; ?></p>
+        <?php else : ?>
+            <p>Artikel tidak ditemukan.</p>
+        <?php endif; ?>
     </div>
 </body>
 </html>
